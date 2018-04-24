2502a14006940089ab1ee9c957579c880da4bb84
huhai <key.hu@bindo.com>
2018-04-24 09:59:12 +0800

    fix all

--- main.go
+++ main.go
@@ -5,10 +5,13 @@ import "log"
 type Attr int
 
 const (
-	Attr_a   Attr = 1
-	Attr_an  Attr = 2
-	Attr_ans Attr = 3
-	Attr_s   Attr = 4
+	Attr_unknow Attr = 0
+	Attr_a      Attr = 1
+	Attr_an     Attr = 2
+	Attr_ans    Attr = 3
+	Attr_z      Attr = 4
+	Attr_n      Attr = 5
+	Attr_b      Attr = 6
 )
 
 type LenType string
@@ -18,21 +21,24 @@ const (
 	Type_VarL   LenType = "VARL"
 	Type_VarLL  LenType = "VARLL"
 	Type_VarLLL LenType = "VARLLL"
+	Type_TLV    LenType = "TLV"
 )
 
 type ISO8583 struct {
-	bInited    bool
-	Fields     map[int]bool
-	ValueMap   map[int]interface{}
-	AttrMap    map[int]Attr
-	LenTypeMap map[int]LenType
+	bInited     bool
+	FieldsArray []int
+	Fields      map[int]bool
+	ValueMap    map[int]interface{}
+	AttrMap     map[int]Attr
+	LenTypeMap  map[int]LenType
 }
 
 func CreateISO8583() *ISO8583 {
 	return &ISO8583{
-		ValueMap:   make(map[int]interface{}, 1),
-		AttrMap:    make(map[int]Attr, 1),
-		LenTypeMap: make(map[int]LenType, 1),
+		ValueMap:   make(map[int]interface{}),
+		AttrMap:    make(map[int]Attr),
+		LenTypeMap: make(map[int]LenType),
+		Fields:     make(map[int]bool),
 		bInited:    true,
 	}
 }
@@ -45,6 +51,7 @@ func (iso *ISO8583) AddField(field int, attr Attr, lenType LenType, value string
 	iso.Fields[field] = true
 	iso.ValueMap[field] = value
 	iso.LenTypeMap[field] = lenType
+	iso.AttrMap[field] = attr
 }
 
 func (iso *ISO8583) AddSubField(parentField, field int, attr Attr, lenType LenType, value string) {
@@ -60,6 +67,7 @@ func (iso *ISO8583) AddSubField(parentField, field int, attr Attr, lenType LenTy
 	subIso.Fields[field] = true
 	subIso.AttrMap[field] = attr
 	subIso.ValueMap[field] = value
+	subIso.LenTypeMap[field] = lenType
 	iso.ValueMap[parentField] = subIso
 }
 
@@ -67,6 +75,7 @@ func (iso *ISO8583) DeleteField(field int) {
 	delete(iso.Fields, field)
 	delete(iso.ValueMap, field)
 	delete(iso.AttrMap, field)
+	delete(iso.LenTypeMap, field)
 }
 
 func (iso *ISO8583) DeleteSubField(parentField, field int) {
@@ -81,37 +90,42 @@ func (iso *ISO8583) DeleteSubField(parentField, field int) {
 	}
 }
 
+//func (iso *ISO8583) bytesLength()int{
+//
+//}
+
 func (iso *ISO8583) valid() bool {
-	if iso.bInited {
+	if !iso.bInited {
 		return false
 	}
 	return true
 }
 
 func (iso *ISO8583) ToJson() []byte {
-
+	return nil
 }
 
 func (iso *ISO8583) ToXml() []byte {
-
+	return nil
 }
 
 func ParseBytes(dataBytes, formatBytes []byte) *ISO8583 {
-
+	return nil
 }
 
 func LoadFromJson(dataBytes []byte) *ISO8583 {
-
+	return nil
 }
 
 func LoadFromXml(dataBytes []byte) *ISO8583 {
-
+	return nil
 }
 
 func Marshal(v interface{}) ([]byte, error) {
 
+	return nil, nil
 }
 
 func Unmarshal(data []byte, v interface{}) error {
-
+	return nil
 }
